a961ba13099312a22cba3eb9c045df8b2d3ef43b
Hchan <hchani95@gamil.com>
2022-05-03 10:18:16 +0900

    project setting

---
+++ src/main/java/com/gg/godgong/GodGongApplication.java
@@ -0,0 +1,13 @@
+package com.gg.godgong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodGongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodGongApplication.class, args);
+    }
+
+}

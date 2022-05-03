75aaa49fd439cb233d9a9d5fed9273f59dec2089
Hchan <hchani95@gamil.com>
2022-05-03 10:53:44 +0900

    react setting

---
+++ src/main/java/com/gg/godgong/HelloController.java
@@ -0,0 +1,16 @@
+package com.gg.godgong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+//리액트를 위한 컨트롤러
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public List<String> Hello(){
+        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+    }
+}

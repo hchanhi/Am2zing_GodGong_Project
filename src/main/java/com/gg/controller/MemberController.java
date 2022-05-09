155bb76e56cf5988ad375333c029d04066c1bc03
Hchan <hchani95@gamil.com>
2022-05-09 10:31:39 +0900

    memberController 메서드 경로 수정

--- src/main/java/com/gg/controller/MemberController.java
+++ src/main/java/com/gg/controller/MemberController.java
@@ -42,7 +42,7 @@ public class MemberController {
 //	}
 
 	//로그인 페이지 이동
-	@GetMapping("/member/login")
+	@GetMapping("/api/member/login")
 	 public String goLogin(HttpServletRequest request) {
         //이전 페이지의 정보
         String url = request.getHeader("Referer");
@@ -53,21 +53,21 @@ public class MemberController {
 		} 
 	
 	/* 로그인 에러 */
-    @PostMapping("member/login")
+    @PostMapping("/api/member/login")
     public String loginError(HttpServletRequest request, Model model) {
         String loginFailMsg = (String) request.getAttribute("loginFailMsg");
         model.addAttribute("loginFailMsg",loginFailMsg);
         return "member/member_loginForm";
     }
 
-	@GetMapping("member/join")
+	@GetMapping("/api/member/join")
 	public String join() {
 
 		return "member/member_joinForm";
 	}
 
     //회원가입 진행
-    @PostMapping("/member/joinProc")
+    @PostMapping("/api/member/joinProc")
     public String joinProc(Member member) {
 
         String rawPassword = member.getPassword();
@@ -79,7 +79,7 @@ public class MemberController {
     }
 
     //마이페이지
-	@GetMapping("member/mypage")
+	@GetMapping("/api/member/mypage")
     public String mypage(Authentication authentication, Model model) {
         Member member = memberService.mypage(authentication.getName());
         model.addAttribute("member",member);
@@ -88,7 +88,7 @@ public class MemberController {
     }
 
 	 //내정보 수정
-    @PostMapping("member/myinfoEdit")
+    @PostMapping("/api/member/myinfoEdit")
     public String memberUpdate(Member member) {
         memberService.updateMember(member);
         return "member/member_mypage";
@@ -96,7 +96,7 @@ public class MemberController {
 
 	//회원가입 이메일 중복 체크
 	@ResponseBody
-    @GetMapping("/member/emailChk")
+    @GetMapping("/api/member/emailChk")
     public HashMap<String, Object> memEmailCheck(@RequestParam(value = "memEmail", required = false) String memEmail) {
 		System.out.println(memberService.memEmailCheck(memEmail));
         return memberService.memEmailCheck(memEmail);
@@ -104,20 +104,20 @@ public class MemberController {
 	
 	//회원가입 닉네임 중복 체크
     @ResponseBody
-    @GetMapping("/member/nicknameChk")
+    @GetMapping("/api/member/nicknameChk")
     public HashMap<String, Object> memNicknameCheck(@RequestParam(value = "memNickname", required = false) String memNickname) {
         return memberService.memNicknameCheck(memNickname);
     }
     
     //닉네임 수정 중복 체크
     @ResponseBody
-    @GetMapping("/member/nicknameEdit")
+    @GetMapping("/api/member/nicknameEdit")
     public HashMap<String, Object> memNicknameEdit(@RequestParam(required = false) Long memId, String memNickname) {
         return memberService.memNicknameEdit(memNickname, memId);
     }
     
     //패스워드 수정페이지
-    @GetMapping("/member/pwdEdit")
+    @GetMapping("/api/member/pwdEdit")
     public String EditPassword(Authentication authentication, Model model) {
     	Member member = memberService.mypage(authentication.getName());
     	model.addAttribute("member",member);
@@ -127,14 +127,14 @@ public class MemberController {
     
     //패스워드 확인
     @ResponseBody
-    @GetMapping("/member/pwdCheck")
+    @GetMapping("/api/member/pwdCheck")
     public HashMap<String, Object> pwCheck(@RequestParam(required = false) String original_Pw, Authentication authentication) {
         return memberService.pwCheck(authentication,original_Pw);
     }
     
 
     //패스워드 변경
-    @PostMapping("/member/pwdEdit")
+    @PostMapping("/api/member/pwdEdit")
     public String pwdUpdate(Authentication authentication,Member member) {
         member.setEmail(authentication.getName());
         memberService.passwordUpdate(member);
@@ -143,7 +143,7 @@ public class MemberController {
 
     
     //회원 탈퇴 실행
-    @PostMapping("/memeber/withdrawal")
+    @PostMapping("/api/memeber/withdrawal")
     public String withdrawalMember(Member member, HttpSession session) {
 
     	memberService.deleteMember(member.getId());

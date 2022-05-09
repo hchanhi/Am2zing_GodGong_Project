bb7e52e173b17b97da5ad23a331a02739d9dc58b
juaekoo <jool0129@gmail.com>
2022-05-09 14:34:39 +0900

    [modify] Making TodoStudyRoom

---
+++ frontend/src/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Modal() {
+
+    return (
+        <div>
+            <h3>모달</h3>
+        </div>
+    );
+}
+
+export default Modal;
--- frontend/src/Todo/TodoStudy.js
+++ frontend/src/Todo/TodoStudy.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import { useParams } from "react-router-dom";
+import Modal from "../Modal";
 
 function TodoStudy() {
 
     let { id } = useParams();
-    // 새로고침으로 인한 state 초기화를 대비하기 위해 Ajax 다시 실행
+
+    let [study, setStudy] = useState({
+        roomCategory: "",
+        roomTitle: "",
+        roomUuid: "",
+        roomCreated: "",
+        roomEntry: "",
+        memberId: ""
+    });
+    let askLeave = () => {
+        if (window.confirm('페이지를 떠나면 이 스터디에서 나가게 돼요. 정말 떠나실 건가요?😥')) {
+            // room DB에서 나의 정보 삭제
+            // 일기작성 모달로 넘어감 (모달직접구현하기)
+            <Modal />
+        } else {
+            return false;
+        }
+    }
+
+    // *참여하기 버튼을 누른 뒤* 페이지를 벗어나는 경우(뒤로가기, 페이지이동, 페이지닫기)
+    let listner = (event) => {
+        event.preventDefault();
+        
+    }
+    window.addEventListener('beforeunload', listner);
+    
+    useEffect(() => {
+        axios.get('/api/todoStudy/', {params: {roomId: id}})
+            .then((res) => {
+                setStudy(res.data);
+                // todo list작성하는 모달 띄움
+            }).catch((error) => {
+                alert('Todo study방의 정보를 가져오는 데 실패했습니다.');
+                console.log(error);
+            });
+    }, [study]);
+    // 다른 스터디원의 실시간 투두 진행상황 보려면 양방향 데이터 통신 필요
 
     return (
         <div>
             <h3>스터디방 id : {id}</h3>
-            스터디만들기 모달창은 라이브러리 쓰지 않고 직접 구현하기<br />
+            <button>참여하기</button>
         </div>
     );
 }

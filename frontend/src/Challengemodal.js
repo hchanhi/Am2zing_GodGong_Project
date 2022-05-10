fe928bbcde9dbe4dfcb59ba1710f9d57ec0f1be9
juhees <103228093+juhees@users.noreply.github.com>
2022-05-11 05:12:42 +0900

    [add] Challengemodal

---
+++ frontend/src/Challengemodal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Challengemodal(){
+
+    return <div className="modalBackground">
+        <div className="modalContainer">
+            <button></button>
+            <div className='title'></div>
+            <div className='body'></div>
+            <div className='footer'></div>
+        </div>
+    </div>
+}
+
+export default Challengemodal

e0014dc7b41cf2e3714c1bad228bcdf0ed7f1105
juhees <juhee2952@gmail.com>
2022-05-11 05:20:20 +0900

    [check] for checking connection

--- frontend/src/Challengemodal.js
+++ frontend/src/Challengemodal.js
@@ -4,7 +4,6 @@ function Challengemodal(){
 
     return <div className="modalBackground">
         <div className="modalContainer">
-            <button></button>
             <div className='title'></div>
             <div className='body'></div>
             <div className='footer'></div>

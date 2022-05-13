61a782e6c7d1b46d8fcf50bc02f7a36a6220edcd
CODE0000 <thfxm0601@gmail.com>
2022-05-13 17:23:24 +0900

    [add]/add loading 추가
    [add]/add loading 추가

--- frontend/src/DiaryList.js
+++ frontend/src/DiaryList.js
@@ -8,15 +8,18 @@ import DiaryButton from './components/DiaryButton';
 import axios from 'axios';
 import './diary.css';
 import DiaryCom from "./components/DiaryCom";
+
 function DiaryList(props) {
 
     const nickname = props.userNickName;
-
+    const [loading, setLoading] = useState(true);
     const [diaries, setDiaries] = useState([]);
     const getDiaries = async () => {
         const json = await axios.get('/api/diary/mydiary', { params: { nickname: nickname } });
         setDiaries(json.data);
         console.log(json.data);
+        console.log(nickname);
+        setLoading(false);
 
     };
     useEffect(() => {
@@ -26,31 +29,40 @@ function DiaryList(props) {
 
 
     return (
+
         <div>
-            <h3>{props.userNickName}님의 마이페이지💁🏻‍♀️</h3>
+            {loading ? (
+                <div >
+                    <h1 >Loading...</h1>
+                </div>
+            ) : (
+                <div>
+                    <h3>{props.userNickName}님의 마이페이지💁🏻‍♀️</h3>
 
-            <h2>공부일기 목록들입니다. 하단에 나올 예정입니다.</h2>
-            <h2></h2>
-            <DiaryButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={'positive'} />
-            <DiaryButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={'negative'} />
-            <DiaryButton text={'버튼'} onClick={() => alert("버튼 클릭")} />
-            <div >
+                    <h2>공부일기 목록들입니다. 하단에 나올 예정입니다.</h2>
+                    <h2></h2>
+                    <DiaryButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={'positive'} />
+                    <DiaryButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={'negative'} />
+                    <DiaryButton text={'버튼'} onClick={() => alert("버튼 클릭")} />
+                    <div >
 
-                {diaries.map((diary) => (
-                    <DiaryCom
-                        key={diary.diaryId}
-                        diaryId={diary.diaryId}
-                        diaryContent={diary.diaryContent}
-                        diarySentiment={diary.diarySentiment}
-                        diaryCreated={diary.diaryCreated.substr(0, 10)}
-
-
-                    />
-
-                ))}
+                        {diaries.map((diary) => (
+                            <DiaryCom
+                                key={diary.diaryId}
+                                diaryId={diary.diaryId}
+                                diaryContent={diary.diaryContent}
+                                diarySentiment={diary.diarySentiment}
+                                diaryCreated={diary.diaryCreated.substr(0, 10)}
 
 
-            </div>
+                            />
+
+                        ))}
+
+
+                    </div>
+                </div>
+            )}
         </div>
     );
 

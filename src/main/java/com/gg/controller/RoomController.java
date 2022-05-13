8ecdbe38de556f51af538d13568fb7e3c066e70b
Hayoung Yu <nhy8001@gmail.com>
2022-05-13 16:12:08 +0900

    [add] create room / room list
    유저 정보 추가

--- src/main/java/com/gg/controller/RoomController.java
+++ src/main/java/com/gg/controller/RoomController.java
@@ -1,8 +1,11 @@
 package com.gg.controller;
 
+import com.gg.config.CurrentUser;
 import com.gg.domain.Room;
 import com.gg.domain.User;
+import com.gg.payload.UserSummary;
 import com.gg.repository.UserRepository;
+import com.gg.service.PrincipalDetails;
 import com.gg.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -27,9 +30,8 @@ public class RoomController {
     }
 
     @PostMapping(value = "/room")
-    public Room create(@RequestParam String roomCategory, @RequestParam String roomTitle) {
-        User user = userRepository.findById(1L).get();
-        log.info("# Create Chat Room, category : " + roomCategory + "title" + roomTitle + "user" + user.getNickname() );
-        return roomService.createRoom(roomCategory, roomTitle, user);
+    public Room create(@RequestParam String roomCategory, @RequestParam String roomTitle, @RequestParam String userNickname) {
+        log.info("# Create Chat Room, category : " + roomCategory + "title" + roomTitle + "user" + userNickname );
+        return roomService.createRoom(roomCategory, roomTitle, userNickname);
     }
 }

119422020c08e5f742ae2ef352669ee506a59bc4
CODE0000 <thfxm0601@gmail.com>
2022-05-14 16:21:54 +0900

    [modify]modify error
    [modify]modify error

--- frontend/src/MyPage.js
+++ frontend/src/MyPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { isAuth, getNickName } from './jwtCheck';
 function MyPage(props) {

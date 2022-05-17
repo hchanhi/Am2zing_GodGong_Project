7eae7a984169d1d0034f8696ee4ec66fd81cffbe
Hchan <hchani95@gamil.com>
2022-05-17 21:56:03 +0900

    [add] refreshtToken setting

---
+++ src/main/java/com/gg/service/RefreshTokenService.java
@@ -0,0 +1,58 @@
+package com.gg.service;
+
+import com.gg.domain.RefreshToken;
+import com.gg.exception.TokenRefreshException;
+import com.gg.repository.RefreshTokenRepository;
+import com.gg.repository.UserRepository;
+import com.sun.security.auth.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RefreshTokenService {
+
+    @Value("${app.jwtRefreshExpirationInMs}")
+    private Long refreshTokenDurationMs;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken createRefreshToken(Authentication authentication) {
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUser(userRepository.findById(principalDetails.getId()).get());
+        refreshToken.setExpiryDate(Instant.now().plusMillis(refreshTokenDurationMs));
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken = refreshTokenRepository.save(refreshToken);
+        return refreshToken;
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new TokenRefreshException(token.getToken(), "Refresh token was expired. Please make a new signin request");
+        }
+        return token;
+    }
+
+    @Transactional
+    public int deleteByUserId(Long userId) {
+        return refreshTokenRepository.deleteByUser(userRepository.findById(userId).get());
+    }
+}

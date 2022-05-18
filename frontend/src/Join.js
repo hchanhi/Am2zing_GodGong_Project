e739fcadbca1d660f59708eac5c9a762793962a9
CODE0000 <thfxm0601@gmail.com>
2022-05-18 11:33:35 +0900

    [add]/add check, user delete
    [add]/add check, user delete

--- frontend/src/Join.js
+++ frontend/src/Join.js
@@ -62,7 +62,7 @@ const Register = () => {
                 if (err.response.status === 400) {
                     alert(err.response.data.message);
                 }
-                setRegisterError('회원가입에 실패하였습니다. 다시한번 확인해 주세요.');
+                setRegisterError('회원가입에 실패하였습니다. 다시한번 확인해 주세요!');
 
             });
     };
@@ -87,7 +87,7 @@ const Register = () => {
         // 이메일 유효성 체크
         // 이메일 유효성 체크
         const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-        if (!emailRegex.test(email)) setEmailError('올바른 이메일 형식이 아닙니다.');
+        if (!emailRegex.test(email)) setEmailError('올바른 이메일 형식이 아닙니다!');
         else setEmailError('');
 
         // 비밀번호 유효성 체크
@@ -100,7 +100,7 @@ const Register = () => {
 
         // 비밀번호 같은지 체크
         if (password !== rePassword) {
-            setPasswordError('비밀번호가 일치하지 않습니다.');
+            setPasswordError('비밀번호가 일치하지 않습니다!');
         } else {
             setPasswordError('');
         }
@@ -108,21 +108,21 @@ const Register = () => {
         // 이름 유효성 검사
         const nameRegex = /^[가-힣|a-zA-Z|0-9]+$/;
         if (!nameRegex.test(nickname) || nickname.length < 1) {
-            setNameError('올바른 이름을 입력해주세요.');
+            setNameError('올바른 닉네임을 입력해주세요!');
         } else {
             setNameError('');
         }
-        // 이름 유효성 검사
+        // 생일 유효성 검사
         const birthRegex = /^[0-9]{6}$/;
         if (!birthRegex.test(birth)) {
-            setBirthError('생년월일을 6자리로 입력해주세요.');
+            setBirthError('생년월일을 6자리로 입력해주세요!');
         } else {
             setBirthError('');
         }
 
 
         // 회원가입 동의 체크
-        if (!checked) alert('회원가입 약관에 동의해주세요.');
+        if (!checked) alert('회원가입 약관에 동의해주세요!');
         if (
             emailRegex.test(email) &&
             passwordRegex.test(password) &&

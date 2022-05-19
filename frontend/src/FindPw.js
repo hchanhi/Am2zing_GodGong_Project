ce3d8233684fd2119d2b69fbef7fa1e7b3eae9c5
구주애 <jool0129@gmail.com>
2022-05-19 12:07:15 +0900

    [Feat] 회원정보 수정 시 토큰 삭제 후 로그아웃

--- frontend/src/FindPw.js
+++ frontend/src/FindPw.js
@@ -10,8 +10,12 @@ function FindPw() {
     useEffect(() => {
         axios.get('/user/password/' + key)
             .then(res => {
-                if (res.success == true)
-                    alert(res.message);
+                if (res.success == true) {
+                    alert('비밀번호 변경이 완료되었습니다. 다시 로그인해주세요.');
+                    localStorage.clear();
+                    // props.setUserNickName('');
+                    navigate('/');
+                }
                 else if (res.message == '유효하지 않은 Key값입니다.')
                     alert('잘못된 접근입니다.');
                 return navigate('/login');

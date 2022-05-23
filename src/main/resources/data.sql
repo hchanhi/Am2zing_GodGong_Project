793d28406552d9d2484a902c47c1936ea4f22b5b
CODE0000 <thfxm0601@gmail.com>
2022-05-23 23:03:49 +0900

    [add]/ 일기 더미데이터 추가
    [add]/ 일기 더미데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,7 +29,10 @@ insert into studylog(studylog_created, studylog_time, user_id) values ("2022-05-
 insert into studylog(studylog_created, studylog_time, user_id) values ("2022-05-23 13:23:13", 2000, 4);
 insert into studylog(studylog_created, studylog_time, user_id) values ("2022-05-23 13:23:13", 10030, 5);
 
-
+insert into diary(diary_content, diary_sentiment, diary_created ,user_id) values("오늘 처음 갓공을 시작했는데 집중이 잘됐다.", "positive", "2022-05-18 13:23:13", 1);
+insert into diary(diary_content, diary_sentiment, diary_created ,user_id) values("작심삼일 중 이틀째! 목표했던걸 모두 끝냈다!", "positive", "2022-05-19 13:23:13", 1);
 insert into diary(diary_content, diary_sentiment, diary_created ,user_id) values("오늘은 공부가 잘됐다.", "positive", "2022-05-20 13:23:13", 1);
+insert into diary(diary_content, diary_sentiment, diary_created ,user_id) values("오늘은 목표의 80%만 이뤘다. 좀 아쉽다. 다음엔 더 잘해야지!", "neutral", "2022-05-21 13:23:13", 1);
+insert into diary(diary_content, diary_sentiment, diary_created ,user_id) values("오늘은 투두리스트를 모두 완료했다! 너무 행복하다!", "positive", "2022-05-22 13:23:13", 1);
 insert into diary(diary_content, diary_sentiment, diary_created ,user_id) values("오늘은 집중이 잘 안되서 힘들었다. 목표했던 만큼 공부를 했어야 하는데 내일은 더 집중할 수 있겠찌?", "negative","2022-05-23 13:23:13", 1);
 

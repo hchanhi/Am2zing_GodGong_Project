ac0d4f79a48f19fff2d30e97c14ca711a9858b30
구주애 <jool0129@gmail.com>
2022-05-23 14:01:06 +0900

    [Fix] context관련 불필요한 코드 삭제

--- frontend/src/Todo/CompleteTodo.js
+++ frontend/src/Todo/CompleteTodo.js
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { RoomNumContext, ClientContext } from './TodoStudyRoom.js';
